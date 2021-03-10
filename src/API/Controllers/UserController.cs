46ac786a3230b028306140077ffc52b5ca8bb039
Joao Grassi <joao@joaograssi.com>
2021-03-10 22:16:18 +0100

    Add missing Authorize attribute Without it the request is not challenged.

--- src/API/Controllers/UserController.cs
+++ src/API/Controllers/UserController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Linq;
 using API.Controllers.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("users")]
     public class UserController : ControllerBase
